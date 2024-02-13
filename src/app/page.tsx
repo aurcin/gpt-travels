fe0cf8ea3dd3538f6cc46089ec191f81f88e7e39
Aurimas Cincikas <aurcin@gmail.com>
2024-02-13 11:30:46 +0200

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link';
+
 export default function HomePage() {
-  return <main>Home Page</main>;
+  return (
+    <main className='hero min-h-screen bg-base-200'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-6xl font-bold text-primary'>GPT Travels</h1>
+          <p className='py-6 text-lg leading-loose'>
+            GPT Travels: Your AI travel planning assistant!
+          </p>
+          <Link href='/chat' className='btn btn-secondary'>
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
