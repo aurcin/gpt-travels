1095752e39194582c908535e9c0f4a4ab15f7a53
Aurimas Cincikas <aurcin@gmail.com>
2024-02-14 06:04:46 +0200

    initial sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from 'next';
+import { FaBarsStaggered } from 'react-icons/fa6';
+
+import Sidebar from '@/components/layout/sidebar';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -10,5 +13,32 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className='drawer lg:drawer-open'>
+      <input
+        type='checkbox'
+        name=''
+        id='my-drawer-2'
+        className='drawer-toggle'
+      />
+
+      <div className='drawer-content'>
+        <label
+          htmlFor='my-drawer-2'
+          className='drawer-button lg:hidden fixed top-6 right-6'>
+          <FaBarsStaggered className='w-8 h-8 text-primary' />
+        </label>
+        <div className='bg-base-200 px-8 py-12 min-h-screen'>{children}</div>
+      </div>
+
+      <div className='drawer-side'>
+        <label
+          htmlFor='my-drawer-2'
+          aria-label='close sidebar'
+          className='drawer-overlay'></label>
+
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
